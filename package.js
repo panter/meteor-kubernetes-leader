89edd25468d90569ae02fdad1515d9369f86b4ca
Marco <macrozone@gmail.com>
2019-05-22 23:10:01 +0200

    fix: add git

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
   summary:
     'Find out which of your pods is the leader in a kubernetes setup. It assumes that you have a k8s.gcr.io/leader-elector sidecar container',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/panter/meteor-kubernetes-leader',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
